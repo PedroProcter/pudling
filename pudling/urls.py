1c124626e811e8a715eda97f730b1a0627f167f4
PedroProcter <OProcter89@gmail.com>
2022-04-06 23:00:04 -0400

    feat(pudling urls): Add booking Paths and Views

--- pudling/urls.py
+++ pudling/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from booking.views import *
 from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', booking),
+    path('rooms/', rooms),
+    path('foodbrands/', food_brands),
 ]
