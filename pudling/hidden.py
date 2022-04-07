234dcad9120ef0278a23edba5664ee026fd7b2ee
PedroProcter <OProcter89@gmail.com>
2022-04-07 08:56:38 -0400

    feat(hidden): Create hidden.py To Store Sensitive Information

---
+++ pudling/hidden.py
@@ -0,0 +1,14 @@
+SECRET_KEY = '<SECRET-KEY>'
+
+ALLOWED_HOSTS = ['<Allowed Hosts>']
+
+DATABASES = {
+    'default': {
+        'ENGINE': '<ENGINE>',
+        'NAME': '<DB>',
+        'USER': '<USERNAME>',
+        'PASSWORD': '<PASSWORD>',
+        'HOST': '<HOST>',
+        'PORT': '<PORT>',
+    }
+}
