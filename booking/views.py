b048b4d1515b772d947886fb15c5e4b1efa2c1ec
PedroProcter <OProcter89@gmail.com>
2022-04-06 22:56:14 -0400

    feat(booking views): Create Views
    Views:
    
    * Booking
    * Rooms
    * Food Brands

--- booking/views.py
+++ booking/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
+def get_all_rooms():
+    return Room.objects.all()
+
+def get_all_food_brands():
+    return FoodBrand.objects.all()
+
+def booking(request):
+    rooms = get_all_rooms()
+    food_brands = get_all_food_brands()
+    return render(request, 'booking.html', {'rooms':rooms, 'food_brands':food_brands})
+
+def rooms(request):
+    return render(request, 'rooms.html')
+
+def food_brands(request):
+    return render(request, 'food_brands.html')
