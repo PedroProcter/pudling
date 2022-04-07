29fb0032e0b051a9c1a28788ea363126d385a52e
PedroProcter <OProcter89@gmail.com>
2022-04-07 00:41:03 -0400

    refactor(models): Normalize Room and FoodBrand
    Small Changes:
    
    * Change decimal_lenght from 4 to 2
    * rename species field to accepted_species
    
    Big Changes:
    
    * Create a ForeignKey field of Room in RoomType (And Eliminate
      ForeignKey of RoomType in Room)
    * Create a ForeignKey field of FoodBrand in FoodBrandType (And Eliminate
      ForeignKey of FoodBrandType in FoodBrand)

--- booking/models.py
+++ booking/models.py
@@ -1,6 +1,4 @@
-import re
 from django.db import models
-from datetime import datetime
 
 # Create your models here.
 class Client(models.Model):
@@ -12,38 +10,37 @@ class Booking(models.Model):
     client = models.ForeignKey(Client, related_name='booked_by', on_delete=models.CASCADE)
     begins_at = models.DateTimeField()
     ends_at = models.DateTimeField()
-    cost = models.DecimalField(max_digits=10, decimal_places=4, default=50.00)
+    cost = models.DecimalField(max_digits=10, decimal_places=2, default=50.00)
     comment = models.TextField(max_length=250)
     canceled = models.BooleanField(default=False)
     payed = models.BooleanField(default=False)
     closed = models.BooleanField(default=False)
 
-class RoomType(models.Model):
-    size = models.TextField(max_length=4)
-    species = models.TextField(max_length=20)
-    description = models.TextField(max_length=100)
-
 class Room(models.Model):
     name = models.TextField(max_length=50, default='Pudling Pet\'s Room')
-    category = models.ForeignKey(RoomType, on_delete=models.CASCADE)
     reserved = models.BooleanField()
     is_active = models.BooleanField()
-    price = models.DecimalField(max_digits=10, decimal_places=4)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+class RoomType(models.Model):
+    room = models.ForeignKey(Room, on_delete=models.PROTECT, default=0)
+    size = models.TextField(max_length=4)
+    accepted_species = models.TextField(max_length=20)
+    description = models.TextField(max_length=100)
 
 class RoomBooking(models.Model):
     room_brand = models.ForeignKey(Room, on_delete=models.CASCADE)
     booking = models.ForeignKey(Booking, on_delete=models.CASCADE)
 
-class FoodType(models.Model):
-    size = models.TextField(max_length=4)
-    species = models.TextField(max_length=20)
-    description = models.TextField(max_length=100)
-
 class FoodBrand(models.Model):
     name = models.TextField(max_length=50)
-    category = models.ForeignKey(FoodType, on_delete=models.CASCADE)
     is_active = models.BooleanField()
-    price = models.DecimalField(max_digits=10, decimal_places=4)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+class FoodType(models.Model):
+    food_brand = models.ForeignKey(FoodBrand, on_delete=models.PROTECT, default=0)
+    accepted_species = models.TextField(max_length=20)
+    description = models.TextField(max_length=100)
 
 class FoodBrandBooking(models.Model):
     food_brand = models.ForeignKey(FoodBrand, on_delete=models.CASCADE)
